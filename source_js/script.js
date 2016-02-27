45ddeb9c1c8b31c46eaefac3347ec2825199a099
Mingwei Hu <calvinmwhu@gmail.com>
2016-02-27 00:21:07 -0600

    add multi-layout section

--- source_js/script.js
+++ source_js/script.js
@@ -1,8 +1,6 @@
 // Write any custom javascript functions here
 
 
-
-
 $(document).ready(function () {
 
     var newwidth = $(window).width();
@@ -26,18 +24,67 @@ $(document).ready(function () {
     //} else {
     //    $("#background-video").css({"width": homewidth});
     //}
-    $("section").css({"min-height": 0.9 * $(window).height()});
+    $("section").css({"min-height": 0.8 * $(window).height()});
+    $(".section-header").css({"min-height": 0.2 * $(window).height()});
 
     //$("#footer-home").css({"min-height": 0.15 * $(window).height()});
 
-    //animate_carousel();
+    animate_carousel();
     //animate_modal();
     //smooth_scroll();
     //navbar_effect_on_scroll();
 });
+//
+//function animate_carousel() {
+//    $(".carousel").slick({
+//        dots: true,
+//        speed: 300
+//    });
+//}
 
 
+function animate_carousel() {
+    var num_carousels = $(".carousel-content").length;
+    for(var i=0; i<num_carousels; i++) {
+        $("#carousel-"+i).removeClass('active');
+    }
 
+    $("#carousel-0").addClass('active');
+
+    $(".carousel-arrow").click(function () {
+        var $curActive = $(".carousel-content.active");
+
+        if ($curActive.length == 0) {
+            console.log("no carousel is active now");
+            $("#carousel-0").addClass('active');
+            $curActive = $(".carousel-0");
+        }
+        var curActiveIdx = parseInt($curActive.attr("id").split('-')[1]);
+        var dir = $(this).attr("id").split('-')[1];
+        var nextIdx = curActiveIdx;
+        if (dir == "l") {
+            nextIdx = curActiveIdx + 1;
+        } else if (dir == "r") {
+            nextIdx = curActiveIdx - 1;
+        }
+        nextIdx = nextIdx % num_carousels;
+        if (nextIdx < 0) nextIdx = num_carousels - 1;
+
+        var $nextActive = $("#carousel-" + nextIdx);
+        $curActive.addClass(dir == "l" ? "go-left" : "go-right");
+        $nextActive.addClass("active").addClass(dir == "l" ? "right-arrive" : "left-arrive");
+        //console.log($curActive);
+        //console.log($nextActive);
+
+        $curActive.one("animationend", function () {
+            $curActive.removeClass("active go-left go-right");
+        });
+        $nextActive.one("animationend", function () {
+            $nextActive.removeClass("right-arrive left-arrive");
+        });
+        return false;
+    });
+}
 
 $(window).load(function () {
     $("#cover-whole-page").hide();
